c976a33fff69f499a4bec6ce9ef3186908b41ede
vickky9ice <65629882+vickky9ice@users.noreply.github.com>
2022-08-25 18:57:45 +0100

    Task2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    if len(sys.argv) == 1:
+        print("{} arguments.".format(len(sys.argv) - 1))
+    elif len(sys.argv) == 2:
+        print("{} argument:".format(len(sys.argv) - 1))
+    else:
+        print("{} arguments:".format(len(sys.argv) - 1))
+    for a in range(len(sys.argv)):
+        if a == 0:
+            continue
+        print("{:d}: {:s}".format(a, sys.argv[a]))
