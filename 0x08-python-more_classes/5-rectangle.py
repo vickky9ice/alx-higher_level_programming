9674025aea15f0f0908a1d016103bb864bcf69f6
vickky9ice <adeboyevictor18@gmail.com>
2022-09-20 22:34:11 +0100

    Task5

--- 0x08-python-more_classes/5-rectangle.py
+++ 0x08-python-more_classes/5-rectangle.py
@@ -1,12 +1,81 @@
 #!/usr/bin/python3
-Rectangle = __import__('5-rectangle').Rectangle
+"""
+A Rectangle
+"""
 
-my_rectangle = Rectangle(2, 4)
-print("Area: {} - Perimeter: {}".format(my_rectangle.area(), my_rectangle.perimeter()))
 
-del my_rectangle
+class Rectangle:
+    """
+    functions and data
+    """
+    def __init__(self, width=0, height=0):
+        """instatiation
+        """
+        self.width = width
+        self.height = height
 
-try:
-    print(my_rectangle)
-except Exception as e:
-    print("[{}] {}".format(e.__class__.__name__, e))
+    @property
+    def width(self):
+        """Getter width
+        """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Setter width
+        """
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Getter height
+        """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Setter height
+        """
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """Returns area
+        """
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """Returns perimeter
+        """
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        else:
+            return 2 * (self.__width + self.__height)
+
+    def __str__(self):
+        """hsajwushw"""
+        if self.__width == 0 or self.__height == 0:
+            return ""
+        else:
+            string = ""
+            for a in range(self.__height):
+                if a == self.__height - 1:
+                    string += self.__width * '#'
+                else:
+                    string += self.__width * '#' + '\n'
+            return string
+
+    def __repr__(self):
+        """Returns a representation of printable string"""
+        return "Rectangle({:d}, {:d})".format(self.__width, self.__height)
+
+    def __del__(self):
+        """ Destructors are called when an object gets destroyed """
+        print("Bye rectangle...")
