df82420cb7c80450226803f9be74de8097dcad35
vickky9ice <adeboyevictor18@gmail.com>
2023-01-17 21:55:06 +0100

    Task3

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,8 +1,6 @@
 #!/usr/bin/node
 const request = require('request');
-const epId = process.argv[2];
-const apiUrl = `http://swapi.co/api/films/${epId}`;
-request.get(apiUrl, (error, response, body) => {
-  if (error) console.log(error);
-  console.log(JSON.parse(body).title);
+let url = 'http://swapi.co/api/films/' + process.argv[2];
+request(url, function (error, response, body) {
+  console.log(error || JSON.parse(body).title);
 });
